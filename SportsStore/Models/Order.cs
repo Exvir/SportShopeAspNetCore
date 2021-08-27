f8849091a7f1f56f8c23028da4c2d3917cbff657
exvir <enotukit@gmail.com>
2021-08-27 20:42:27 +0300

    Модель заказа

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Name { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string Cite { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        [Required(ErrorMessage = "Please enter a country name")]
+        public string Country { get; set; }
+        public bool GiftWrap { get; set; }
+    }
+}
